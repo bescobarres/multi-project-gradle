16af074e6b3152d0daee88fa87065243af76a612
Bryan <bescobarres@gmail.com>
2020-06-06 17:42:05 -0500

    [master]: SpringBoot multi-project with gradle

---
+++ src/test/java/com/company/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,12 @@
+package com.company.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
